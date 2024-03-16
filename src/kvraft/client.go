201df13a57b75c9d587eddf8aba37385bef05853
zag13 <zs8153@163.com>
2024-03-16 10:57:09 +0800

    init 3A

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,18 @@
 package kvraft
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
-
+import (
+	"6.5840/labrpc"
+	"crypto/rand"
+	"math/big"
+	"time"
+)
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	leaderId  int
+	clientId  int64
+	requestId int64
 }
 
 func nrand() int64 {
@@ -21,6 +26,11 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+
+	ck.leaderId = -1
+	ck.clientId = nrand()
+	ck.requestId = 0
+
 	return ck
 }
 
@@ -35,9 +45,47 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
+	ck.requestId++
+	args := GetArgs{
+		Key:       key,
+		ClientId:  ck.clientId,
+		RequestId: ck.requestId,
+	}
 
-	// You will have to modify this function.
-	return ""
+	for {
+		serversToTry := []int{ck.leaderId}
+		if ck.leaderId == -1 {
+			serversToTry = make([]int, len(ck.servers))
+			for i := range ck.servers {
+				serversToTry[i] = i
+			}
+		}
+
+		for _, serverId := range serversToTry {
+			reply := GetReply{}
+			ok := ck.servers[serverId].Call("KVServer.Get", &args, &reply)
+			if !ok {
+				continue
+			}
+			switch reply.Err {
+			case OK:
+				ck.leaderId = serverId
+				// DPrintf("Get: key %v, value %v", key, reply.Value)
+				return reply.Value
+			case ErrNoKey:
+				ck.leaderId = serverId
+				// DPrintf("Get: key %v does not exist", key)
+				return ""
+			case ErrWrongLeader:
+				// DPrintf("Get: wrong leader %v", serverId)
+			default:
+				// DPrintf("Get: unexpected error %v", reply.Err)
+			}
+		}
+
+		time.Sleep(100 * time.Millisecond)
+		ck.leaderId = -1
+	}
 }
 
 // shared by Put and Append.
@@ -49,7 +97,46 @@ func (ck *Clerk) Get(key string) string {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-	// You will have to modify this function.
+	ck.requestId++
+	args := PutAppendArgs{
+		Key:       key,
+		Value:     value,
+		Op:        op,
+		ClientId:  ck.clientId,
+		RequestId: ck.requestId,
+	}
+
+	for {
+		serversToTry := []int{ck.leaderId}
+		if ck.leaderId == -1 {
+			serversToTry = make([]int, len(ck.servers))
+			for i := range ck.servers {
+				serversToTry[i] = i
+			}
+		}
+
+		for _, serverId := range serversToTry {
+			reply := PutAppendReply{}
+			ok := ck.servers[serverId].Call("KVServer.PutAppend", &args, &reply)
+			// DPrintf("PutAppend: serverId %v, ok %v, args %v, reply %v", serverId, ok, args, reply)
+			if !ok {
+				continue
+			}
+			switch reply.Err {
+			case OK:
+				ck.leaderId = serverId
+				// DPrintf("PutAppend: key %v, value %v", key, value)
+				return
+			case ErrWrongLeader:
+				// DPrintf("PutAppend: wrong leader %v", serverId)
+			default:
+				// DPrintf("PutAppend: unexpected error %v", reply.Err)
+			}
+		}
+
+		time.Sleep(100 * time.Millisecond)
+		ck.leaderId = -1
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -1,12 +1,13 @@
 package kvraft
 
 import (
-	"6.5840/labgob"
-	"6.5840/labrpc"
-	"6.5840/raft"
 	"log"
 	"sync"
 	"sync/atomic"
+
+	"6.5840/labgob"
+	"6.5840/labrpc"
+	"6.5840/raft"
 )
 
 const Debug = false
@@ -18,11 +19,15 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Type      string // "Put", "Append" or "Get"
+	Key       string
+	Value     string
+	ClientId  int64
+	RequestId int64
 }
 
 type KVServer struct {
@@ -35,15 +40,103 @@ type KVServer struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	db                map[string]string // key-value database
+	applyCond         *sync.Cond
+	lastAppliedIndex  int             // last applied index
+	lastAppliedTerm   int             // last applied term
+	clientLastRequest map[int64]int64 // key: clientId, value: last request id
 }
-
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	if _, isLeader := kv.rf.GetState(); !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	op := Op{
+		Type:      "Get",
+		Key:       args.Key,
+		Value:     "",
+		ClientId:  args.ClientId,
+		RequestId: args.RequestId,
+	}
+
+	index, term, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	success := kv.waitApply(index, term)
+	if !success {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	kv.applyCond.L.Lock()
+	defer kv.applyCond.L.Unlock()
+
+	val, ok := kv.db[args.Key]
+	if !ok {
+		reply.Err = ErrNoKey
+		return
+	}
+
+	reply.Err = OK
+	reply.Value = val
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	if _, isLeader := kv.rf.GetState(); !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	kv.applyCond.L.Lock()
+	lastReqId, ok := kv.clientLastRequest[args.ClientId]
+	if ok && args.RequestId <= lastReqId {
+		kv.applyCond.L.Unlock()
+		reply.Err = OK
+		return
+	}
+	kv.applyCond.L.Unlock()
+
+	op := Op{
+		Type:      args.Op,
+		Key:       args.Key,
+		Value:     args.Value,
+		ClientId:  args.ClientId,
+		RequestId: args.RequestId,
+	}
+
+	index, term, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+	// DPrintf("server %d start: %v index: %v term: %v", kv.me, op, index, term)
+
+	success := kv.waitApply(index, term)
+	if !success {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	reply.Err = OK
+}
+
+func (kv *KVServer) waitApply(index int, term int) bool {
+	kv.applyCond.L.Lock()
+	defer kv.applyCond.L.Unlock()
+
+	for !kv.killed() && kv.lastAppliedIndex < index {
+		kv.applyCond.Wait()
+	}
+
+	b := kv.lastAppliedIndex >= index && kv.lastAppliedTerm == term
+	return b
 }
 
 // the tester calls Kill() when a KVServer instance won't
@@ -87,11 +180,39 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.maxraftstate = maxraftstate
 
 	// You may need initialization code here.
+	kv.db = make(map[string]string)
+	kv.applyCond = sync.NewCond(&kv.mu)
+	kv.lastAppliedIndex = 0
+	kv.lastAppliedTerm = 0
+	kv.clientLastRequest = make(map[int64]int64)
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
 	// You may need initialization code here.
+	go kv.applyListener()
 
 	return kv
+}
+
+func (kv *KVServer) applyListener() {
+	for msg := range kv.applyCh {
+		DPrintf("server %d receive apply msg: %v", kv.me, msg)
+		kv.applyCond.L.Lock()
+		if msg.CommandValid && msg.CommandIndex > kv.lastAppliedIndex {
+			op := msg.Command.(Op)
+			switch op.Type {
+			case "Put":
+				kv.db[op.Key] = op.Value
+			case "Append":
+				kv.db[op.Key] += op.Value
+			}
+
+			kv.applyCond.Broadcast()
+			kv.lastAppliedIndex = msg.CommandIndex
+			kv.lastAppliedTerm = msg.CommandTerm
+			kv.clientLastRequest[op.ClientId] = op.RequestId
+		}
+		kv.applyCond.L.Unlock()
+	}
 }
